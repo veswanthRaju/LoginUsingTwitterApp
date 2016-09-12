aee76579a0c97fe67ab064c5ce579464380f063d
Veswanth Raju <veswanthraju.thokachichu@walkingtree.tech>
2016-09-12 17:13:56 +0530

    Hiding Key values

--- SampleFbLoginApp/Constants.cs
+++ SampleFbLoginApp/Constants.cs
@@ -5,8 +5,8 @@
         public static string wait = "Please wait..";
         public static string info = "Checking user Info..";
         
-        public static string TwitterCosnumerKey = "1dBZNVp9L4JWZ02YGGr4Cj02F";
-        public static string TwitterConsumerSecret = "QhCKalzT56kjlbucfkfjw7Jyt66VFVt0AEd2D0q2ZdwbnD1lhm";        
+        public static string TwitterCosnumerKey = "Give your Consumer Key here";
+        public static string TwitterConsumerSecret = "Give your consumer secret key here";        
         public static string reqToken = "https://api.twitter.com/oauth/request_token";
         public static string twitterAuthorize = "https://api.twitter.com/oauth/authorize";
         public static string accessToken = "https://api.twitter.com/oauth/access_token";
